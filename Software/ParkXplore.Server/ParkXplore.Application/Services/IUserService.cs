236a12e1644a865b1e3e27e4d6ae768502b2d104
Karla Kulier <38597@DESKTOP-4Q3G9S3>
2024-11-23 00:59:05 +0100

    Define IUserService interface using User entity.

---
+++ Software/ParkXplore.Server/ParkXplore.Application/Services/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using ParkXplore.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkXplore.Application.Services {
+    public interface IUserService {
+        Task<User?> GetUserByIdAsync(int userId);
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task<bool> CreateUserAsync(User user);
+        Task<bool> UpdateUserAsync(User user);
+        Task<bool> DeleteUserAsync(int userId);
+
+        Task<User?> LoginAsync(string email, string password);
+    }
+}
